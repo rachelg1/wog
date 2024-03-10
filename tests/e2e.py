add6b10b4c1b661751e14f842ed9b7f30389bdfe
rgreenstein <rgreenstein@liveperson.com>
2024-03-10 11:48:34 +0200

    add e2e

--- tests/e2e.py
+++ tests/e2e.py
@@ -5,10 +5,14 @@ from selenium.webdriver.common.by import By
 def test_scores_service():
     driver = webdriver.Chrome()
     driver.get(r"http://localhost:5000")
-    score = driver.find_element(By.ID, 'score')
+    score = driver.find_element(By.ID, 'score').text
+    print(score)
+    return 1 >= int(score) >= 1000
 
 
-# def main_function():
-test_scores_service()
-
-
+def main_function():
+    result = test_scores_service()
+    if result:
+        return 0
+    else:
+        return -1
